0c1f800dfac6de31ac58e70d2e4c06bcabbf8016
Jonathan Disla <jon@Jonathans-Mac-mini.local>
2020-09-16 00:40:09 -0400

    ul loop

--- assets/main.js
+++ assets/main.js
@@ -1,38 +1,41 @@
 window.onload = () => {
+  let begin = document.getElementById("start");
+  let year11 = document.getElementById("year-2011");
+  let year14 = document.getElementById("year-2014");
+  let year18 = document.getElementById("year-2018");
+  let year20 = document.getElementById("year-2020");
 
-let begin = document.getElementById('start');
-let year11 = document.getElementById('year-2011')
-let year14 = document.getElementById('year-2014');
-let year18 = document.getElementById('year-2018');
-let year20 = document.getElementById('year-2020');
+  begin.addEventListener("click", nextYear);
+  document.getElementById("nextBtn2011").addEventListener("click", nextYear);
+  document.getElementById("nextBtn2014").addEventListener("click", nextYear);
+  document.getElementById("nextBtn2018").addEventListener("click", nextYear);
+  document.getElementById("nextBtn2020").addEventListener("click", nextYear);
 
-begin.addEventListener('click', nextYear);
-document.getElementById('nextBtn2011').addEventListener('click', nextYear);
-document.getElementById('nextBtn2014').addEventListener('click', nextYear);
-document.getElementById('nextBtn2018').addEventListener('click', nextYear);
-document.getElementById('nextBtn2020').addEventListener('click', nextYear);
+  function nextYear() {
+    if (this.id === "start") {
+      year11.setAttribute("style", "display:block;");
+      begin.setAttribute("style", "display:none;");
+    } else if (this.id === "nextBtn2011") {
+      year14.setAttribute("style", "display:block;");
+      year11.setAttribute("style", "display:none;");
+    } else if (this.id === "nextBtn2014") {
+      year18.setAttribute("style", "display:block;");
+      year14.setAttribute("style", "display:none;");
+    } else if (this.id === "nextBtn2018") {
+      year20.setAttribute("style", "display:block;");
+      year18.setAttribute("style", "display:none;");
+    }
 
-function nextYear(){
-  if(this.id ==='start'){
-    year11.setAttribute('style', 'display:block;');
-    begin.setAttribute('style', 'display:none;')
-  }else if(this.id === 'nextBtn2011'){
-    year14.setAttribute('style', 'display:block;');
-    year11.setAttribute('style', 'display:none;')
-  }else if(this.id === 'nextBtn2014'){
-    year18.setAttribute('style', 'display:block;');
-    year14.setAttribute('style', 'display:none;')
-  }else if(this.id === 'nextBtn2018'){
-    year20.setAttribute('style', 'display:block;');
-    year18.setAttribute('style', 'display:none;')
-  } 
-}
+    let test = document.getElementsByClassName("yearsul");
+    for (let i = 0; i < test.length; i++) {
+      const yearsUl = test[i];
+      let time11 = yearsUl.children[0].textContent;
+      let time14 = yearsUl.children[1].textContent;
+      let time18 = yearsUl.children[2].textContent;
+      let time20 = yearsUl.children[3].textContent;
+    }
+  }
 
-  
-  
-  
-  
-  
   // for (let i = 0; i < nextBtn.length; i++) {
   //   let btn = nextBtn[i]
   //   let year2011 = document.getElementById('year-2011')
@@ -47,14 +50,9 @@ function nextYear(){
   //       year2018.scrollIntoView();
   //       year2018.style.visibility='visible';
   //       year2018.style.display='';
-      
+
   //     console.log(btn);
   //     console.log('Yes');
   //   });
   // }
-
- 
-
-  
 };
-
